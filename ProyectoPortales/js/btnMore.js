1877cb41411cc2c0075beaa85ce11aab2da3a2cb
ClaudioRamos <claudioarh504@gmail.com>
2026-08-02 12:06:55 -0600

    Servicios completado

--- ProyectoPortales/js/btnMore.js
+++ ProyectoPortales/js/btnMore.js
@@ -4,8 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
         moreButton.addEventListener("click", () => {
             const servicio = moreButton.closest(".servicio");
             const serviceBody = servicio.querySelector(".service-body");
+            const serviceTitle = servicio.querySelector(".service-title");
             serviceBody.classList.toggle("hidden");
             moreButton.classList.toggle("minimize");
+            serviceTitle.classList.toggle("active");
         });
     });
 });
