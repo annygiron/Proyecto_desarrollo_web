b907339c1099a0a4ea979b562f89d835d72dea41
GeorgeJJR <jorge.jaredrv@gmail.com>
2026-08-02 20:10:07 -0600

    modificación en btnMore

--- ProyectoPortales/js/btnMore.js
+++ ProyectoPortales/js/btnMore.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
     const moreButtons = document.querySelectorAll(".more-button");
     moreButtons.forEach((moreButton) => {
         moreButton.addEventListener("click", () => {
@@ -8,6 +8,23 @@ document.addEventListener("DOMContentLoaded", () => {
             serviceBody.classList.toggle("hidden");
             moreButton.classList.toggle("minimize");
             serviceTitle.classList.toggle("active");
+        });
+    });
+});*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const serviceTitles = document.querySelectorAll('.service-title');
+
+    serviceTitles.forEach(title => {
+        title.addEventListener('click', () => {
+            const servicio = title.closest('.servicio');
+            const serviceBody = servicio.querySelector('.service-body');
+            const moreButton = title.querySelector('.more-button');
+            serviceBody.classList.toggle('hidden');
+            servicio.classList.toggle('active');
+            if (moreButton) {
+                moreButton.classList.toggle('minimize');
+            }
         });
     });
 });
