2e275fc38284e6e322c902e4500dc50650109884
GeorgeJJR <jorge.jaredrv@gmail.com>
2026-08-05 00:25:10 -0600

    creacion del hmb button

--- ProyectoPortales/js/main.js
+++ ProyectoPortales/js/main.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     // Instanciando el Carusel
     let caruselInstance = new Carusel(".carusel");
+
+    let hmbButton = document.querySelector("header .hmb-button");
+    let nav = document.querySelector("header nav");
+    hmbButton.addEventListener("click", (e)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        nav.classList.toggle("hidden");
+    });
 });
